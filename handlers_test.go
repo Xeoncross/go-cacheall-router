0e66542c9947561862d885a07bf45b5608f65f34
xeoncross <david@xeoncross.com>
2020-02-20 15:34:01 -0600

    more work to fix mux routing

--- handlers_test.go
+++ handlers_test.go
@@ -4,9 +4,11 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/xeoncross/go-cacheall-router/internal/gorilla"
 )
 
-func TestRawHandlers(t *testing.T) {
+func TestHandlers(t *testing.T) {
 
 	testCases := []struct {
 		desc     string
@@ -28,7 +30,7 @@ func TestRawHandlers(t *testing.T) {
 		},
 		{
 			desc:     "javascript",
-			path:     "/js/app.js",
+			path:     "/static/js/app.js",
 			handler:  http.FileServer(MemoryHTTPFilesystem.Dir("build")),
 			response: JavascriptResponse,
 		},
@@ -49,7 +51,42 @@ func TestRawHandlers(t *testing.T) {
 			if rr.Body.String() != tc.response {
 				t.Errorf("got: %q\nwant: %q\n", rr.Body.String(), tc.response)
 			}
+
 		})
+	}
+
+	//
+	// Now test libraries
+	//
+
+	handlers := []struct {
+		desc    string
+		handler http.Handler
+	}{
+		{
+			desc:    "gorilla/mux/",
+			handler: gorilla.Handler(MemoryHTTPFilesystem.Dir("build"), APIHandler),
+		},
+	}
+
+	for _, tc := range testCases {
+		for _, handler := range handlers {
+
+			t.Run(handler.desc+tc.desc, func(t *testing.T) {
+				req, err := http.NewRequest("GET", tc.path, nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				rr := httptest.NewRecorder()
+
+				handler.handler.ServeHTTP(rr, req)
+
+				if rr.Body.String() != tc.response {
+					t.Errorf("got: %q\nwant: %q\n", rr.Body.String(), tc.response)
+				}
+			})
+		}
 	}
 
 }
--- internal/gorilla/gorilla.go
+++ internal/gorilla/gorilla.go
@@ -3,20 +3,22 @@ package gorilla
 import (
 	"net/http"
 	"net/url"
-
-	gocacheallrouter "github.com/xeoncross/go-cacheall-router"
 )
 
 // Handler returns gorilla/mux handler
-func Handler() http.Handler {
+func Handler(dir http.FileSystem, api http.HandlerFunc) http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/api", http.HandlerFunc(gocacheallrouter.APIHandler))
-	mux.Handle("/build/", http.StripPrefix("/build/", http.FileServer(gocacheallrouter.MemoryHTTPFilesystem)))
+	mux.HandleFunc("/api", http.HandlerFunc(api))
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(dir)))
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		// Overwrite URL???
-		r.URL = &url.URL{Path: "index.html"}
-		http.FileServer(gocacheallrouter.MemoryHTTPFilesystem).ServeHTTP(w, r)
+		// r.URL = &url.URL{Path: "index.html"}
+
+		u, _ := url.Parse("index.html")
+		r.URL = u
+
+		http.FileServer(dir).ServeHTTP(w, r)
 
 		// Pull from memory object as io.Reader??
 		// w.Header().Set("Content-Type", "text/html; charset=utf-8")
